6b0c96c333e9b2e7891e776f86f3b74499c69842
Martin <tinroldandg@gmail.com>
2021-03-03 13:31:24 -0500

    Raycast en el dedo

---
+++ Assets/Scripts/VR/Prubeas/ScriptsInteractuar/RayCastDedo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastDedo : MonoBehaviour
+{
+
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1, Color.green);
+    }
+}
