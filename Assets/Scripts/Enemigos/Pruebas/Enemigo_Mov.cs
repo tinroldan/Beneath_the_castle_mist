2c33e674af32b84de748eb4669e2119cb0444917
Manuel <mfvelsaa@hotmail.com>
2021-03-04 13:51:38 -0500

    Mejora Ia enemigo

---
+++ Assets/Scripts/Enemigos/Pruebas/Enemigo_Mov.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class Enemigo_Mov : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public GameObject target;                                    // target to aim for
+
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+            target=GameObject.FindWithTag("Player");
+        }
+
+
+        private void Update()
+        {
+            if (target != null)
+                agent.SetDestination(target.transform.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+                character.Move(Vector3.zero, false, false);
+        }
+
+
+        public void SetTarget(GameObject target)
+        {
+            this.target = target;
+        }
+    }
+}
